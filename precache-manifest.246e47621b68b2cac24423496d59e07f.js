88963f7a295351e0af72bd09184fb0728b915468
pavel <leskovecpavel7@gmail.com>
2019-06-30 22:54:14 +0300

    Updates

---
+++ precache-manifest.246e47621b68b2cac24423496d59e07f.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9126fcf67cf4aef348b9823b059bffd9",
+    "url": "/animation-player/index.html"
+  },
+  {
+    "revision": "de388e8d512ab82b7b12",
+    "url": "/animation-player/static/css/main.055abc55.chunk.css"
+  },
+  {
+    "revision": "74a4fd0efb8e8a68622e",
+    "url": "/animation-player/static/js/2.ab3af549.chunk.js"
+  },
+  {
+    "revision": "de388e8d512ab82b7b12",
+    "url": "/animation-player/static/js/main.667defe7.chunk.js"
+  },
+  {
+    "revision": "97b338dcf49579d98186",
+    "url": "/animation-player/static/js/runtime~main.58aee25c.js"
+  },
+  {
+    "revision": "57300bcc434a7ad0893b3c36b036a354",
+    "url": "/animation-player/static/media/bat.57300bcc.gif"
+  },
+  {
+    "revision": "5601f86fe8928676688bd32f17f99651",
+    "url": "/animation-player/static/media/cat_landing.5601f86f.gif"
+  },
+  {
+    "revision": "e8432507d0cb4654d034e34ce3f20e11",
+    "url": "/animation-player/static/media/chessboard.e8432507.png"
+  },
+  {
+    "revision": "d36919317a74a46f7756cd9050d28b7a",
+    "url": "/animation-player/static/media/example.d3691931.png"
+  },
+  {
+    "revision": "26c200992dde11612d46eab7ae395b29",
+    "url": "/animation-player/static/media/linkedin.26c20099.png"
+  },
+  {
+    "revision": "466b422c6a1ce24a6c4e20e62832d72a",
+    "url": "/animation-player/static/media/mario.466b422c.gif"
+  }
+]);
